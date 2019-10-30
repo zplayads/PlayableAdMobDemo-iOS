33df9f1f0596078c78899b230a27a0607ff1f7db
JiaDingYi <wzy2010416033@163.com>
2019-10-30 11:49:14 +0800

    add banner view controller

---
+++ PlayableAdMobDemo/BannerViewController.h
@@ -0,0 +1,17 @@
+//
+//  BannerViewController.h
+//  PlayableAdMobDemo
+//
+//  Created by 王泽永 on 2019/10/30.
+//  Copyright © 2019 playable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BannerViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PlayableAdMobDemo/BannerViewController.m
@@ -0,0 +1,13 @@
+//
+//  BannerViewController.m
+//  PlayableAdMobDemo
+//
+//  Created by 王泽永 on 2019/10/30.
+//  Copyright © 2019 playable. All rights reserved.
+//
+
+#import "BannerViewController.h"
+
+@implementation BannerViewController
+
+@end
