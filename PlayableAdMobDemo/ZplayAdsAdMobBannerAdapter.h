7943fd1ca68c92101da8b31161edd01c53fe8eac
JiaDingYi <wzy2010416033@163.com>
2019-10-30 12:08:44 +0800

    create zplay ads admob banner adapter

---
+++ PlayableAdMobDemo/ZplayAdsAdMobBannerAdapter.h
@@ -0,0 +1,17 @@
+//
+//  ZplayAdsAdMobBannerAdapter.h
+//  PlayableAdMobDemo
+//
+//  Created by 王泽永 on 2019/10/30.
+//  Copyright © 2019 playable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZplayAdsAdMobBannerAdapter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PlayableAdMobDemo/ZplayAdsAdMobBannerAdapter.m
@@ -0,0 +1,13 @@
+//
+//  ZplayAdsAdMobBannerAdapter.m
+//  PlayableAdMobDemo
+//
+//  Created by 王泽永 on 2019/10/30.
+//  Copyright © 2019 playable. All rights reserved.
+//
+
+#import "ZplayAdsAdMobBannerAdapter.h"
+
+@implementation ZplayAdsAdMobBannerAdapter
+
+@end
